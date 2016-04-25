c9cfa6e83db9dbb5c6347697c94799b8cb939966
marcoscaballerojorge <marcos.caballero.jorge@alumnos.upm.es>
2016-04-25 17:24:45 +0200

    Tema4: No perder respuesta al intentar

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,21 @@
-
 // GET /question
-exports.question = function(req, res, next) {
-   res.render('quizzes/question', {question: 'Capital de Italia'});
-};
+ exports.question = function(req, res, next) {
 
-// GET /check
-exports.check = function(req, res, next) {
-	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result});
-};
+
+	var answer = req.query.answer || '';
+
+	res.render('quizzes/question', {question: 'Capital de Italia',
+								    answer: answer});
+ };
+ 
+ // GET /check
+ exports.check = function(req, res, next) {
+
+
+	var answer = req.query.answer || "";
+
+	var result = answer === 'Roma' ? 'Correcta' : 'Incorrecta';
+
+	res.render('quizzes/result', { result: result, 
+								   answer: answer });
+ };
