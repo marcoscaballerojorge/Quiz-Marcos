7b5c1e5d0244c6801d4564ae5d18cadc5b0d5120
marcoscaballerojorge <marcos.caballero.jorge@alumnos.upm.es>
2016-05-02 14:00:44 +0200

    Tema16: Borrar preguntas

--- quiz10/routes/index.js
+++ quiz10/routes/index.js
@@ -1,23 +1,27 @@
- var express = require('express');
- var router = express.Router();
- 
+var express = require('express');
+var router = express.Router();
+
 var quizController = require('../controllers/quiz_controller');
 
- /* GET home page. */
- router.get('/', function(req, res, next) {
-   res.render('index');
- });
-
- router.get('/author', function(req, res, next) {
-res.render('author');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
 });
-  // Autoload de rutas que usen :quizId
+
+
+// Autoload de rutas que usen :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+
 // Definición de rutas de /quizzes
 router.get('/quizzes',                     quizController.index);
 router.get('/quizzes/:quizId(\\d+)',       quizController.show);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
+router.get('/quizzes/new',                 quizController.new);
+router.post('/quizzes',                    quizController.create);
+router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
+router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
-router.get('/quizzes/new', quizController.new);
-router.post('/quizzes', quizController.create);
- module.exports = router;
+
+module.exports = router;
